caf023eecbfce15ee6f173265ae20e62b7ff951b
RoksanaBo <roksana85@hotmail.com>
2020-06-02 16:43:01 -0600

    added classes

--- src/test/java/com/bookit/runners/CucumberRunner.java
+++ src/test/java/com/bookit/runners/CucumberRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         plugin = {
                 "json:target/cucumber.json"
         },
-//        tags = "@create_student_3",
+     //   tags = "@create_student_3",
         dryRun = false
 
 )
